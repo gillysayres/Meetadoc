47e1fa031444dc776e32d74a36da7a66327c5f95
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-20 09:42:49 -0300

    add seed file v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-puts 'Destroying all previous data'
+puts 'Destroying all previous data...'
 Doctor.destroy_all
+Patient.destroy_all
 
 #CRM é formado somente por números seguido da Sigla do Estado ex: 0000/SP minímo 4 dígitos ou 0000000000/SP máximo 10 dígitos
+puts 'Creating doctors...'
 Doctor.create!(
   name: 'John',
   crm: 3232,
@@ -21,3 +23,21 @@ Doctor.create!(
   name: 'James',
   crm: 6451123,
   crm_uf: 'BA')
+
+puts 'Creating patients...'
+
+Patient.create!(
+  name: 'Noah',
+  cpf: '371.555.435-51')
+
+Patient.create!(
+  name: 'Oliver',
+  cpf: '336.590.400-02')
+
+Patient.create!(
+  name: 'Elijah',
+  cpf: '995.204.960-92')
+
+Patient.create!(
+  name: 'William',
+  cpf: '767.760.999-60')
