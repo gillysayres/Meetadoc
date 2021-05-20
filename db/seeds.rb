0022898770438c1cceec327f74bbd460e606ada6
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-19 21:37:33 -0300

    add seed file v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying all previous data'
+Doctor.destroy_all
+
+#CRM é formado somente por números seguido da Sigla do Estado ex: 0000/SP minímo 4 dígitos ou 0000000000/SP máximo 10 dígitos
+Doctor.create!(
+  name: 'John',
+  crm: 3232,
+  crm_uf: 'SP')
+
+Doctor.create!(
+  name: 'Jake',
+  crm: 3246332,
+  crm_uf: 'BA')
+
+Doctor.create!(
+  name: 'Josh',
+  crm: 3232,
+  crm_uf: 'MG')
+
+Doctor.create!(
+  name: 'James',
+  crm: 6451123,
+  crm_uf: 'BA')
