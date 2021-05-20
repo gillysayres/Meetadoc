98b3e39761de4545a565d07dedb368df33e4bd3e
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-20 09:27:52 -0300

    add private methods and before action

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,7 +1,17 @@
 class PatientsController < ApplicationController
+  before_action :set_patient, only: [:show, :edit, :destroy]
 
   def index
     @patients = Patient.all
   end
 
+  private
+
+  def patient_params
+    params.require(:patient).permit(:name, :birth_date, :cpf)
+  end
+
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
 end
