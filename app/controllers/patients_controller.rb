b0ca477bf1872b587b8ca02ed652331e6cc2729a
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-20 10:35:02 -0300

    add edit action and view

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,5 @@
 class PatientsController < ApplicationController
-  before_action :set_patient, only: [:show, :edit, :destroy]
+  before_action :set_patient, only: [:show, :edit, :update, :destroy]
 
   def index
     @patients = Patient.all
@@ -11,9 +11,20 @@ class PatientsController < ApplicationController
   def create
     @patient = Patient.new(patient_params)
     if @patient.save
-      redirect_to root_path
+      redirect_to patients_path
     else
       render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @patient.update(patient_params)
+      redirect_to patients_path
+    else
+      render :new
     end
   end
 
