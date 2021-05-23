c11e355fef0da371be558854b56d46ff079b99f5
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-23 16:31:00 -0300

    add minor tweaks

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ApplicationRecord
   validates_uniqueness_of :cpf
   validates :name, :cpf, :birth_date, presence: true
-  validates :cpf, format: { with: /(\d{3})(\d{3})(\d{3})(\d{2})/ }
+  validates :cpf, format: { with: /(\d{3})(\d{3})(\d{3})(\d{2})/, message: "This CPF is invalid!" }
 end
