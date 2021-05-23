b1bae28fefab1107d3ffdfb5799d4f44f496f64d
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-23 16:59:55 -0300

    rspec for patient model

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates a patient' do
+    patient = Patient.new(
+      name: 'John Doe',
+      cpf: '532.345.501-52',
+      birth_date: 1988-05-20
+      )
+    expect(patient.name).to eql "John Doe"
+    expect(patient.cpf).to eql "532.345.501-52"
+    expect(patient.birth_date).to eql 1988-05-20
+  end
 end
