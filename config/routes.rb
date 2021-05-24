964d1de99cabb3930df2326fa18b435e557a09b8
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-24 20:16:59 -0300

    add root to dashboardhome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'dashboard#home'
+
   resources :doctors, except: [:destroy]
   delete "doctors/:id", to: "doctors#destroy", as: :destroy
 
