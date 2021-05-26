157497b5b7b7fc149add00f4ac1e6aae806e021d
Gillys Dantas <gillys.ayres@gmail.com>
2021-05-26 18:18:59 -0300

    adds several tweaks to dashboards front

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,22 +1,17 @@
 class DashboardController < ApplicationController
 
   def home
-    # @patients = Appointment.where
     @doctors = Doctor.all
+    @appointments_all = Appointment.all.count
     @appointments = Appointment.all.where("ends_at <= ?", DateTime.now).count
-    @unique_patients = Appointment.all.where("ends_at <= ?", DateTime.now).count
-    @unique_patients = Appointment.all.distinct("ends_at <= ? AND patients_id", DateTime.now).count
-    #   SELECT DISTINCT
-    @appointment_all = Appointment.all
-    # SELECT COUNT (ID) FROM appointments, where ends_at < DateTime.now AND id = ?
-
-    # SELECT COUNT (ID) FROM appointments, where ends_at < DateTime.now, GROUP_BY ends_at
-    # SELECT COUNT (patient_id) FROM appointments, where ends_at < DateTime.now, GROUP_BY patient_id
-    a = DateTime.now
+    @appointment_by_doctor = Appointment.all.where("ends_at <= ? AND doctor_id = ?", DateTime.now, 9).count
+    @unique_patients =  Appointment.distinct(:patient_id).where("ends_at <= ?", DateTime.now).size
     # @appointment = Appointment.select(:id).where(:ends_at < a).group(:ends_at)
     # @appointment = Appointment.select(:id).where("ends_at <= ?", DateTime.now)
-    @appointments2 = Appointment.all.where("ends_at <= ? AND id = ?", DateTime.now, 2).count
-    @appointment_by_doctor = Appointment.all.where("ends_at <= ? AND doctor_id = ?", DateTime.now, 9).count
-    # @appointment2 = @appointment.count
   end
 end
+
+    # SELECT COUNT (ID) FROM appointments, where ends_at < DateTime.now AND id = ?
+    # SELECT COUNT (ID) FROM appointments, where ends_at < DateTime.now, GROUP_BY ends_at
+    # SELECT COUNT (patient_id) FROM appointments, where ends_at < DateTime.now, GROUP_BY patient_id
+    # SELECT DISTINCT
